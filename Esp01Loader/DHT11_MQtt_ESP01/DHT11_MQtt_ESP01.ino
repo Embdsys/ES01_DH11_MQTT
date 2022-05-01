1d6ef45dc1440e56ed2a0e677689f415a19874ec
embdsys <embdsys>
2022-05-01 01:02:41 -0700

    Robusted node red, takes "outTopic" as sensor name in sqlite

--- Esp01Loader/DHT11_MQtt_ESP01/DHT11_MQtt_ESP01.ino
+++ Esp01Loader/DHT11_MQtt_ESP01/DHT11_MQtt_ESP01.ino
@@ -7,11 +7,11 @@
 #define DHTPIN 2     // Digital pin connected to the DHT sensor
 // Bash variables to set on cmd / terminal
 
-//$ssid
-//$wifiPassword
-//$mqttServerIp
-//$inTopic
-//$outTopic
+//DBB5DF
+//3HW149W30E11F
+//192.168.0.77
+//inTopic2
+//outTopic2
 
 // Uncomment the type of sensor in use:
 #define DHTTYPE    DHT11     // DHT 11
@@ -30,9 +30,9 @@ WiFiUDP ntpUDP;
 // no offset
 NTPClient timeClient(ntpUDP);
 // Update these with values suitable for your network.
-const char* ssid = "$ssid"; //"DBB5DF";  // $ssid
-const char* password = "$wifiPassword";//"3HW149W30E11F";  // $wifiPassword
-const char* mqtt_server = "$mqttServerIp";//"192.168.0.77"; // $mqttServerIp
+const char* ssid = "DBB5DF"; //"DBB5DF";  // DBB5DF
+const char* password = "3HW149W30E11F";//"3HW149W30E11F";  // 3HW149W30E11F
+const char* mqtt_server = "192.168.0.77";//"192.168.0.77"; // 192.168.0.77
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -93,7 +93,7 @@ void reconnect() {
       // Once connected, publish an announcement...
       //client.publish("outTopic", "hello world");
       // ... and resubscribe
-      client.subscribe("$inTopic"); //Subsribe to a topic here $inTopic
+      client.subscribe("inTopic2"); //Subsribe to a topic here inTopic2
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -130,6 +130,6 @@ void loop() {
     int tempo = millis(); 
     //Gettime
     snprintf (msg, MSG_BUFFER_SIZE, "%i : %i : %i",tempo, tempValue, humValue); //Here you write the message to publish
-    client.publish("$outTopic",msg); // Here is out topic $outTopic
+    client.publish("outTopic2",msg); // Here is out topic outTopic2
   }
 }
--- Esp01Loader/loadTempSensor.sh
+++ Esp01Loader/loadTempSensor.sh
@@ -39,19 +39,8 @@ echo "Done updating file!!"
 
 cat ./DHT11_MQtt_ESP01/DHT11_MQtt_ESP01.ino
 
-#xdotool key ctrl+shift+t
-arduino ./DHT11_MQtt_ESP01/DHT11_MQtt_ESP01.ino
-#xdotool key ctrl+shift+t
-bash runBinary.sh
-# Make .ino into .bin
-#WID=`xdotool search --name "arduino" | head -1`
-#xdotool selectwindow
-# Make the fucker click on the screen to select it
-# left half screen x:416 y:445 screen:0 window:37748780
-# wait to let arduino IDE load
-#sleep 120
-#xdotool mousemove 416 445 click 1
-#xdotool keydown ctrl+alt+s
-#xdotool keyup ctrl+alt+s
+echo "Open ./DHT11_MQtt_ESP01/DHT11_MQtt_ESP01.ino and make bind file"
+read -p "Press key to continue.. " -n1 -s
 
-echo "stage1 completed!!"
+#esptool --port /dev/ttyUSB0 write_flash 0x1000 DHT11_MQtt_ESP01/DHT11_MQtt_ESP01.ino.nodemcu.bin 
+echo "Done loading"
